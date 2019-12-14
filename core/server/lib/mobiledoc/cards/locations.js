e2bc9408d639f05fc7564ceaa5b383162d37dfec
iLoveBug <fan.zhang@me.com>
2019-12-14 21:14:19 +0800

    locations卡片渲染支持多个实例，支持从固定ID改为从css类名kg-locations-container查找元素

--- core/server/lib/mobiledoc/cards/locations.js
+++ core/server/lib/mobiledoc/cards/locations.js
@@ -1,5 +1,36 @@
 const createCard = require('../create-card');
 
+function uuid(len, radix) {
+    let chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split('');
+    let uuid = [];
+    let i;
+    radix = radix || chars.length;
+
+    if (len) {
+        // Compact form
+        for (i = 0; i < len; i++) {
+            uuid[i] = chars[0 | Math.random() * radix];
+        }
+    } else {
+        // rfc4122, version 4 form
+        let r;
+
+        // rfc4122 requires these characters
+        uuid[8] = uuid[13] = uuid[18] = uuid[23] = '-';
+        uuid[14] = '4';
+
+        // Fill in random data.  At i==19 set the high bits of clock sequence as
+        // per rfc4122, sec. 4.1.5
+        for (i = 0; i < 36; i++) {
+            if (!uuid[i]) {
+                r = 0 | Math.random() * 16;
+                uuid[i] = chars[(i === 19) ? (r & 0x3) | 0x8 : r];
+            }
+        }
+    }
+    return uuid.join('');
+}
+
 module.exports = createCard({
     name: 'locations',
     type: 'dom',
@@ -14,12 +45,13 @@ module.exports = createCard({
 
         let dom = opts.env.dom;
         let div = dom.createElement('div');
-        div.setAttribute('id', 'locations-view-wrapper');
+        div.setAttribute('class', 'kg-locations-container');
         div.setAttribute('style', 'width:100%;');
 
+        let dataId = uuid();
         let data = Buffer.from(opts.payload.locations).toString('base64');
         let code = dom.createElement('code');
-        code.setAttribute('id', 'location-view-data');
+        code.setAttribute('id', dataId);
         code.setAttribute('style', 'display: none;');
         code.appendChild(dom.createRawHTMLSection(data));
         div.appendChild(code);
@@ -28,7 +60,7 @@ module.exports = createCard({
         locationComponent.setAttribute('width', '100%');
         locationComponent.setAttribute('height', '500px');
         locationComponent.setAttribute('access-key', '8325164e247e15eea68b59e89200988b');
-        locationComponent.setAttribute('data', 'location-view-data');
+        locationComponent.setAttribute('data', dataId);
         locationComponent.setAttribute('default-icon', '/assets/poi-marker-default.png');
         locationComponent.setAttribute('closed-icon', '/assets/poi-marker-red.png');
 
